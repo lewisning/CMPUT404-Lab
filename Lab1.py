87f4c77383f2d20ec4fac32c431cfceead6dcc0f
Lewis <36083021+lewisning@users.noreply.github.com>
2022-01-12 17:24:45 -0700

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -4,5 +4,6 @@ import requests
 print(requests.__version__)
 
 # 2. Get the information of google homepage
-print(requests.get("https://google.com"))
+homepage = requests.get("https://google.com")
+print(homepage.text)
 
